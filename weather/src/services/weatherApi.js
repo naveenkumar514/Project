9f743dd1dd667d27d12cc972deb0bea014fc85f3
Naveen <naveenbhardwaj2206@gmail.com>
2026-02-15 00:23:33 +0530

    update fav icon

--- weather/src/services/weatherApi.js
+++
@@ -1,7 +0,0 @@
-export const getWeather = async (lat, lon) => {
-  const res = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto`
-  );
-
-  return await res.json();
-};
